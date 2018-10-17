ecb12d3fa161a97c2d4557a70d8804d7d76ec7f3
Emir Hadzimehmedovic <emirhadzimehmedovic@yahoo.com>
2018-10-17 16:15:22 +0200

    Prvi commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author emirh
+ *
+ */
+module Kalkulator {
+}
---
+++ src/novipaackage/Kalkulator.java
@@ -0,0 +1,15 @@
+package novipaackage;
+
+public class Kalkulator {
+	int broj1;
+	int broj2;
+
+	public Kalkulator(int broj1, int broj2) {
+		this.broj1 = broj1;
+		this.broj2 = broj2;
+	}
+
+	public int sabiranje(int broj1, int broj2) {
+		return broj1 + broj2;
+	}
+}
